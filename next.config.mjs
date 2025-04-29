33526abce13b8f9605828b86e72e5dee657e8926
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2025-04-29 19:14:02 +0600

    client file

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    SERVER_URL: "http://localhost:5050",
+    // SERVER_URL: "https://server.rizvienp.top",
+  },
+};
+
+export default nextConfig;
